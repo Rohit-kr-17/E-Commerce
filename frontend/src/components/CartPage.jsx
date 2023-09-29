8f5c7f1563e146e285c07ad890919a9fb02a698d
Rohit Kumar <82036216+Rohit-kr-17@users.noreply.github.com>
2023-09-29 11:19:01 +0530

    Update CartPage.jsx

--- frontend/src/components/CartPage.jsx
+++ frontend/src/components/CartPage.jsx
@@ -37,7 +37,7 @@ function CartPage() {
 
 	const getToken = async () => {
 		try {
-			const { data } = await axios.get(client.get("/product/braintree/token"));
+			const { data } = await client.get("/product/braintree/token");
 			setClientToken(data?.clientToken);
 		} catch (err) {
 			console.log(err);
@@ -106,13 +106,11 @@ function CartPage() {
 				<div className="mt-2">
 					<h4>Total: {totalPrice()}</h4>
 				</div>
-				<div>
+				<div className="flex w-64 h-32 flex-wrap">
 					<DropIn
 						options={{
-							authorization: clientToken,
-							paypal: {
-								flow: "vault",
-							},
+							authorization:
+								"eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiJleUowZVhBaU9pSktWMVFpTENKaGJHY2lPaUpGVXpJMU5pSXNJbXRwWkNJNklqSXdNVGd3TkRJMk1UWXRjMkZ1WkdKdmVDSXNJbWx6Y3lJNkltaDBkSEJ6T2k4dllYQnBMbk5oYm1SaWIzZ3VZbkpoYVc1MGNtVmxaMkYwWlhkaGVTNWpiMjBpZlEuZXlKbGVIQWlPakUyT1RZd05USTBOVFVzSW1wMGFTSTZJbVUwTURaa1pEQXdMV0l3T0RNdE5EY3daQzA1TWpobUxXVXhOVEl6TWpNeE1qQm1PU0lzSW5OMVlpSTZJalExYm5oeU5IcHdiV3B3Wkdaa00yTWlMQ0pwYzNNaU9pSm9kSFJ3Y3pvdkwyRndhUzV6WVc1a1ltOTRMbUp5WVdsdWRISmxaV2RoZEdWM1lYa3VZMjl0SWl3aWJXVnlZMmhoYm5RaU9uc2ljSFZpYkdsalgybGtJam9pTkRWdWVISTBlbkJ0YW5Ca1ptUXpZeUlzSW5abGNtbG1lVjlqWVhKa1gySjVYMlJsWm1GMWJIUWlPbVpoYkhObGZTd2ljbWxuYUhSeklqcGJJbTFoYm1GblpWOTJZWFZzZENKZExDSnpZMjl3WlNJNld5SkNjbUZwYm5SeVpXVTZWbUYxYkhRaVhTd2liM0IwYVc5dWN5STZlMzE5LjR3SWwwdmRsbGdzWlZIUTdDT1pQaWhMbG1rcFBseVZZaEFzQ2ViYl83NW9CZTFEei1uekZPQm5TYzZUSlYtaUdVWV9vSVc5c2lDbEhoZGhOaEFqT05nIiwiY29uZmlnVXJsIjoiaHR0cHM6Ly9hcGkuc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbTo0NDMvbWVyY2hhbnRzLzQ1bnhyNHpwbWpwZGZkM2MvY2xpZW50X2FwaS92MS9jb25maWd1cmF0aW9uIiwiZ3JhcGhRTCI6eyJ1cmwiOiJodHRwczovL3BheW1lbnRzLnNhbmRib3guYnJhaW50cmVlLWFwaS5jb20vZ3JhcGhxbCIsImRhdGUiOiIyMDE4LTA1LTA4IiwiZmVhdHVyZXMiOlsidG9rZW5pemVfY3JlZGl0X2NhcmRzIl19LCJjbGllbnRBcGlVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvNDVueHI0enBtanBkZmQzYy9jbGllbnRfYXBpIiwiZW52aXJvbm1lbnQiOiJzYW5kYm94IiwibWVyY2hhbnRJZCI6IjQ1bnhyNHpwbWpwZGZkM2MiLCJhc3NldHNVcmwiOiJodHRwczovL2Fzc2V0cy5icmFpbnRyZWVnYXRld2F5LmNvbSIsImF1dGhVcmwiOiJodHRwczovL2F1dGgudmVubW8uc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbSIsInZlbm1vIjoib2ZmIiwiY2hhbGxlbmdlcyI6WyJjdnYiXSwidGhyZWVEU2VjdXJlRW5hYmxlZCI6dHJ1ZSwiYW5hbHl0aWNzIjp7InVybCI6Imh0dHBzOi8vb3JpZ2luLWFuYWx5dGljcy1zYW5kLnNhbmRib3guYnJhaW50cmVlLWFwaS5jb20vNDVueHI0enBtanBkZmQzYyJ9LCJwYXlwYWxFbmFibGVkIjp0cnVlLCJwYXlwYWwiOnsiYmlsbGluZ0FncmVlbWVudHNFbmFibGVkIjp0cnVlLCJlbnZpcm9ubWVudE5vTmV0d29yayI6dHJ1ZSwidW52ZXR0ZWRNZXJjaGFudCI6ZmFsc2UsImFsbG93SHR0cCI6dHJ1ZSwiZGlzcGxheU5hbWUiOiJSb2hpdCIsImNsaWVudElkIjoiYXNkZmFkc2Zhc2RmYXNkZiIsImJhc2VVcmwiOiJodHRwczovL2Fzc2V0cy5icmFpbnRyZWVnYXRld2F5LmNvbSIsImFzc2V0c1VybCI6Imh0dHBzOi8vY2hlY2tvdXQucGF5cGFsLmNvbSIsImRpcmVjdEJhc2VVcmwiOm51bGwsImVudmlyb25tZW50Ijoib2ZmbGluZSIsImJyYWludHJlZUNsaWVudElkIjoibWFzdGVyY2xpZW50MyIsIm1lcmNoYW50QWNjb3VudElkIjoicm9oaXQiLCJjdXJyZW5jeUlzb0NvZGUiOiJVU0QifX0=",
 						}}
 						onInstance={(instance) => {
 							setInstance(instance);
